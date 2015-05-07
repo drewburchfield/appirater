ffc390a74643b953009e2f5d7910b1332aba8d1c
Patrick Goley <patrick.goley@gmail.com>
2015-05-07 12:18:34 -0500

    added missing files

---
+++ FARatingPromptable.h
@@ -0,0 +1,15 @@
+//
+//  FARatingPromptable.h
+//  FestappCore
+//
+//  Created by Patrick Goley on 5/7/15.
+//  Copyright (c) 2015 aloompa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FARatingPromptable <NSObject>
+
+@property (nonatomic, strong) NSNumber *ratingPromptWeight;
+
+@end
